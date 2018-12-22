ca0acc1cb653982951bb7fbe7671bcc3dd6167d4
greenplugin <wformps@gmail.com>
2018-12-23 04:23:58 +0700

    Added SendMessageMethod

---
+++ src/Method/SendMessageMethod.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Greenplugin\TelegramBot\Method;
+
+/**
+ * Class SendMessageMethod
+ * @link https://core.telegram.org/bots/api#sendmessage
+ */
+class SendMessageMethod extends SendMethodAbstract
+{
+    /**
+     * Text of the message to be sent
+     *
+     * @var string
+     */
+    public $text;
+
+    /**
+     * Optional. Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width
+     * text or inline URLs in the media caption.
+     *
+     * @var string|null
+     */
+    public $parseMode;
+
+    /**
+     * Optional. Disables link previews for links in this message
+     *
+     * @var boolean|null
+     */
+    public $disableWebPagePreview;
+}
