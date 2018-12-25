fd1748acf9fa39d7337553ed8fb2f062cd091021
Big Shark <bigshark666@gmail.com>
2018-12-26 01:55:08 +0300

    Added simple type test

---
+++ tests/Type/UserTypeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenplugin\TelegramBot\Tests\Type;
+
+use Greenplugin\TelegramBot\Type\UserType;
+
+class UserTypeTest extends TypeTestCase
+{
+    public function testEncode()
+    {
+        $result = [
+            'id' => 1,
+            'is_bot' => true,
+            'first_name' => 'test',
+            'last_name' => 'test',
+            'username' => 'test',
+            'language_code' => 'en',
+        ];
+        $botApi = $this->getBot($result);
+
+        $type = $botApi->call(new \stdClass(), UserType::class);
+
+        $this->assertEquals($type->id, $result['id']);
+        $this->assertEquals($type->isBot, $result['is_bot']);
+        $this->assertEquals($type->firstName, $result['first_name']);
+        $this->assertEquals($type->lastName, $result['last_name']);
+        $this->assertEquals($type->username, $result['username']);
+        $this->assertEquals($type->languageCode, $result['language_code']);
+    }
+}
